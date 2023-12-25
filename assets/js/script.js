5e73b0f4662f5be72e9e0614710c9e327dc54ca1
Redifo <kaanredif@gmail.com>
2023-12-25 15:05:41 +0100

    Add hide class, Add event listener for goig back to home page from game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
-
+    let navImg = document.getElementById("nav-img");
+    // Add a click event listener to the nav-img
+    navImg.addEventListener("click", function() {
+        alert("Are you sure you want to go to Home Page? Your game score will reset");
+    });
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
